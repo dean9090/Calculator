59c04e3c6cfd9fd6693bada8a3366e83744308c8
Dejan Saric <dsaric@mail.usf.edu>
2017-07-14 14:09:53 -0400

    Start of JS

---
+++ work.js
@@ -0,0 +1,19 @@
+
+const number = document.querySelector(".Number");
+const operator = document.querySelector(".operator");
+const clear = document.querySelector(".Clear");
+const screen = document.querySelector(".Screen");
+const equal = document.querySelector(".Equal");
+
+
+let operations = "";
+
+for (var i = 0; i < number.length; i++) {
+  let curretVal = number[i];
+}
+
+
+
+
+
+//
