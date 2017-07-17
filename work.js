cc5062a350da8b745d0b185f6fec9904a0e36b1a
Dejan Saric <dsaric@mail.usf.edu>
2017-07-17 17:16:35 -0400

    finished calculator

--- work.js
+++ work.js
@@ -8,42 +8,70 @@ let equal = document.querySelector(".Equal");
 
 let operations = "";
 
+let number1 = "";
+let calc = "";
+let number2 = "";
+
+
+
 for (var i = 0; i < number.length; i++) { // looping though numbers
   let currentVal = number[i];     // saving the number into a variable
 
-  currentVal.addEventListener("click", () =>{ // adding event listener to the value
+  currentVal.addEventListener("click",  function(){ // adding event listener to the value
     let text = currentVal.innerText;          // takes the value and creates it as innerText
-    operations += text;                       // assign this to operations so it puts it in the value of it
+    operations += text;                      // assign this to operations so it puts it in the value of it
     viewer.textContent = operations;          // assign operations to viewer
-  })
-}
 
-for (var i = 0; i < operator.length; i++) {
-  let math = operator[i];
-
-  math.addEventListener("click", function(){
-    let show = math.innerText;
-
-    if (show === "X") {
-      operations += "*"
+    if (text === "+" || text === "-" || text === "X" || text === "/") {
+      calc += text;
     }
-    else {operations += show;}
-    viewer.textContent = operations;
+    else {
+      if (number1 !== "" && calc !==""){
+        number2+= text;
+      } else {
+        number1 += text;
+      }
+    }
+    console.log(number1);
+    console.log(calc);
+    console.log(number2);
   })
 }
+
+
+
+
 
 clear.addEventListener("click" , function() {
+
+// RESETS ALL OF THE VARIABLES TO ZERO
+
     operations = "";
+    number1 = "";
+    calc = "";
+    number2 = "";
     viewer.textContent = operations;
 })
 
 equal.addEventListener("click" , function () {
-    let program = eval(operations).toString();            // 
+    let program = 0;
+    switch (calc) {
+      case "+":
+        program = Number(number1) + Number(number2);
+        break;
+      case "-":
+        program = Number(number1) - Number(number2);
+        break;
+      case "X":
+        program = Number(number1) * Number(number2);
+        break;
+      case "/":
+        program = Number(number1) / Number(number2)
+        break;
+      }
     viewer.textContent = program;
 
 })
-
-
 
 
 
